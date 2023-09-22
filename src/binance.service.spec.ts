b096fb33bb54f98edc3cfebe69f87af4d8e3adf9
Alexander Los <los.alex@ilink.dev>
2023-09-22 18:25:41 +0600

    add unit test for sign logic

--- src/binance.service.spec.ts
+++ src/binance.service.spec.ts
@@ -51,7 +51,35 @@ describe('BinanceService', () => {
     })
 
     describe('applyDeposit', () => {
-        it('should succeed on request', async () => {
+        it('should sign the request', async () => {
+            const timestamp = 1695294113617
+
+            let requestUrl
+
+            jest.spyOn(binanceService, 'request').mockImplementation(
+                (request) => {
+                    requestUrl = request.url
+                    return binanceService.request(request)
+                },
+            )
+            jest.spyOn(Date, 'now').mockImplementation(() => timestamp)
+
+            try {
+                await binanceService.applyDeposit({
+                    subAccountId: '681164088293367800',
+                    txId: 'c051d9cdc010f931158c3040a097930f9e520153fc19b7fe51aeb9716c996e59',
+                    depositId: 3644705731255021600,
+                })
+            } catch (error) {
+                console.error(error)
+            }
+
+            expect(requestUrl).toBe(
+                '/sapi/v1/capital/deposit/credit-apply?timestamp=1695294113617&signature=ccafb59cb90f8690c58a6e209a5ef9513bd751018af29eb6f2f853ad5786c09a',
+            )
+        })
+
+        it.skip('should succeed on the request', async () => {
             expect(
                 (
                     await binanceService.applyDeposit({
